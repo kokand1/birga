a57ed0c84091c7153232986b8a1e0b5442b0a9bc
ayubxonn <ayyubkasimov@gmail.com>
2023-06-24 15:43:21 +0500

    elevated

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,1 +1,20 @@
-//hamma narsa uchip ketdi uxwadi
+import 'package:flutter/material.dart';
+
+void main () => runApp(MaterialApp(
+  home: kk(),
+));
+
+class kk extends StatefulWidget {
+  const kk({super.key});
+
+  @override
+  State<kk> createState() => _kkState();
+}
+
+class _kkState extends State<kk> {
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: ElevatedButton(onPressed: (){}, child: Text('Next')),);
+  }
+}
